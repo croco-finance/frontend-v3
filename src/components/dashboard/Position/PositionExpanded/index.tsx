da851b0913d8e87169e6348bb84a94fb5aa263d9
robertbrada <bert.blader@gmail.com>
2021-06-28 16:26:02 +0200

    feat: expanded info initial layout

--- src/components/dashboard/Position/PositionExpanded/index.tsx
+++ src/components/dashboard/Position/PositionExpanded/index.tsx
@@ -3,27 +3,55 @@ import React from 'react'
 import styled from 'styled-components'
 import { useExpandedData } from 'state/dashboard/hooks'
 import Loader from 'components/Loader'
+import DailyFeesChart from 'components/dashboard/Position/PositionExpanded/DailyFeesChart.tsx'
+import { usePoolDatas } from 'state/pools/hooks'
+import DensityChart from 'components/DensityChart'
+import { LightCard } from 'components/Card'
 
 const Wrapper = styled.div`
   display: flex;
   width: 100%;
-  background-color: ${({ theme }) => theme.bg0};
-  padding: 10px;
-  border-radius: 10px;
-  align-items: center;
-  font-weight: ${({ theme }) => theme.fontWeight.medium};
+  margin-top: 20px;
+  ${({ theme }) => theme.mediaWidth.upToMedium`
+  flex-direction: column;
+  `};
+`
+
+const DailyFeesWrapper = styled(LightCard)`
+  width: 50%;
+  margin-right: 10px;
+  ${({ theme }) => theme.mediaWidth.upToMedium`
+  width: 100%;
+  margin: 0 0 5px 0;
+  `};
+`
+const DensityChartWrapper = styled(LightCard)`
+  width: 50%;
+  margin-left: 10px;
+  ${({ theme }) => theme.mediaWidth.upToMedium`
+  width: 100%;
+  margin: 5px 0 0 0;
+  `};
 `
 
 interface Props {
   tokenId: number
   owner: string
+  poolAddress: string
 }
 
-const PositionExpanded = ({ owner, tokenId }: Props) => {
+const PositionExpanded = ({ tokenId, owner, poolAddress }: Props) => {
   const theme = useTheme()
   const expandedInfo = useExpandedData(owner, tokenId)
-  console.log('PositionExpanded, expandedInfo: ', expandedInfo)
-  return <Wrapper>{expandedInfo ? <div>{expandedInfo.collectedFeesToken0}</div> : <Loader />}</Wrapper>
+  // liquidity distribution data
+  const poolData = usePoolDatas([poolAddress])[0]
+
+  return (
+    <Wrapper>
+      <DailyFeesWrapper>{expandedInfo?.dailyFees ? <div>DAILY FEES</div> : <Loader />}</DailyFeesWrapper>
+      <DensityChartWrapper>{poolData ? <DensityChart address={poolAddress} /> : <Loader />}</DensityChartWrapper>
+    </Wrapper>
+  )
 }
 
 export default PositionExpanded
